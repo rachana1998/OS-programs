a82eb5b2d43e78c96905cfbbaf038d40ed154ade
rachana1998 <rachana2602@gmail.com>
2020-10-20 22:29:30 +0530

    system call demo

---
+++ syscalldemo/syscalldemo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<unistd.h>
+
+int main()
+{
+int fd;
+fd = open("sys.c",O_WRONLY|O_CREAT);
+if(fd==-1)
+{
+perror("error in open\n");
+return -1;
+}
+write(fd,"cdac",4);
+close(fd);
+return 0;
+}
+

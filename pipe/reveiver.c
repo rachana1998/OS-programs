1d5784c6475334acc6641197fbb735f4036aaa24
rachana1998 <rachana2602@gmail.com>
2020-10-21 19:52:47 +0530

    data transfer between files in normal form

---
+++ pipe/reveiver.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+int main()
+{
+int fd;
+unsigned char rbuff[50];
+fd=open("desd",O_RDONLY);
+read(fd,rbuff,50);
+printf("data received from sender is %s\n",rbuff);
+close(fd);
+return 0;
+}
